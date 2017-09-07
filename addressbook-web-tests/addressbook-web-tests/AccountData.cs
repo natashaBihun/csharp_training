111056b8c41f66a3ccedb8f52fc514ab3d14600d
Natasha Bihun <bigynnatasha@gmail.com>
2017-09-07 19:52:11 +0300

    Created GroupData and AccountData classes

---
+++ addressbook-web-tests/addressbook-web-tests/AccountData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class AccountData
+    {
+        private string _username;
+        private string _password;
+
+        public string Username {
+            get {
+                return _username;
+            }
+            set {
+                _username = value;
+            }
+        }
+        public string Password {
+            get {
+                return _password;
+            }
+            set {
+                _password = value;
+            }
+        }
+
+        public AccountData(string username, string password) {
+            _username = username;
+            _password = password;
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class GroupData
+    {
+        private string _name;
+        private string _header = "";
+        private string _footer = "";
+
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                _name = value;
+            }
+        }
+        public string Header {
+            get {
+                return _header;
+            }
+            set {
+                _header = value;
+            }
+        }
+        public string Footer {
+            get {
+                return _footer;
+            }
+            set {
+                _footer = value;
+            }
+        }
+
+        public GroupData(string name) {
+            _name = name;
+        }
+        public GroupData(string name, string header, string footer)
+        {
+            _name = name;
+            _header = header;
+            _footer = footer;
+        }
+    }
+}
