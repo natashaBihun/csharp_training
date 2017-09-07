1bcbc8a4d621b48cb6c12cacf9ab9d896a26c9c5
Natasha Bihun <bigynnatasha@gmail.com>
2017-09-07 21:15:51 +0300

    Created Contact creation tests

---
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests/ContactCreationTests.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = @"C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+            options.UseLegacyImplementation = true;
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            InitContactCreation();
+
+            ContactData contactData = new ContactData() {
+                FirstName = "Natasha",
+                LastName = "Bihun",
+                Title = "Contact info",
+                Company = "SharpMinds",
+                Address = "Ukraine",
+                HomePhone = "0978271133",
+                MobilePhone = "0978271138",
+                Email = "nbihun@sharpminds.com",
+                BDay = "20",
+                BMonth = "October",
+                BYear = "1991",
+                NameOfGroup = "name"
+            };
+
+            FillContactForm(contactData);
+            SubmitContactCreation();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void SubmitContactCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+        private void FillContactForm(ContactData contactData)
+        {
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contactData.FirstName);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contactData.LastName);
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(contactData.Title);
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(contactData.Company);
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(contactData.Address);
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(contactData.HomePhone);
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(contactData.MobilePhone);
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(contactData.Email);
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(contactData.BDay);
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(contactData.BMonth);
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(contactData.BYear);
+            new SelectElement(driver.FindElement(By.Name("new_group"))).SelectByText(contactData.NameOfGroup);
+            // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
+        }
+
+        private void InitContactCreation()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests/ContactData.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    public class ContactData
+    {
+        private string _firstName;
+        private string _lastName = "";
+        private string _title = "";
+        private string _company = "";
+        private string _address = "";
+        private string _homePhone = "";
+        private string _mobilePhone = "";
+        private string _email = "";
+        private string _bDay = "";
+        private string _bMonth = "";
+        private string _bYear = "";
+        private string _nameOfGroup = "";
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+        public string Company
+        {
+            get
+            {
+                return _company;
+            }
+            set
+            {
+                _company = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return _address;
+            }
+            set
+            {
+                _address = value;
+            }
+        }
+        public string HomePhone
+        {
+            get
+            {
+                return _homePhone;
+            }
+            set
+            {
+                _homePhone = value;
+            }
+        }
+        public string MobilePhone
+        {
+            get
+            {
+                return _mobilePhone;
+            }
+            set
+            {
+                _mobilePhone = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+        public string BDay
+        {
+            get
+            {
+                return _bDay;
+            }
+            set
+            {
+                _bDay = value;
+            }
+        }
+        public string BMonth
+        {
+            get
+            {
+                return _bMonth;
+            }
+            set
+            {
+                _bMonth = value;
+            }
+        }
+        public string BYear
+        {
+            get
+            {
+                return _bYear;
+            }
+            set
+            {
+                _bYear = value;
+            }
+        }
+        public string NameOfGroup
+        {
+            get
+            {
+                return _nameOfGroup;
+            }
+            set
+            {
+                _nameOfGroup = value;
+            }
+        }
+
+        public ContactData() { }
+
+        public ContactData(string firstName) {
+            _firstName = firstName;
+        }
+        public ContactData(
+            string firstName, string lastName,  string title,  string company, string address, string homePhone, 
+            string mobilePhone, string email, string bDay, string bMonth, string bYear, string nameOfGroup) {
+
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = title;
+            _company = company;
+            _address = address;
+            _homePhone = homePhone;
+            _mobilePhone = mobilePhone;
+            _email = email;
+            _bDay = bDay;
+            _bMonth = bMonth;
+            _bYear = bYear;
+            _nameOfGroup = nameOfGroup;
+
+        }
+    }
+}
