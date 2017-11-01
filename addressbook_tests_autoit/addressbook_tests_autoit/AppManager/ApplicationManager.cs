6e85cfd2265fdfbfbc46a7ed06cb709a3e48be3d
Natasha Bihun <bigynnatasha@gmail.com>
2017-11-01 20:09:39 +0200

    New project

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/AppManager/ApplicationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+        public ApplicationManager() { }
+        public void Stop() {
+
+        }
+    }
+}
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ef24546-26ae-4122-902d-0dc7185557c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
